48f7b59b3d88b2a207fba2e41f1df377276cfab8
Patrick MARIE <p.marie@criteo.com>
2022-01-08 18:06:31 +0100

    Exercism's go track: first exercises (update from existing)

--- exercism/go/lasagna/lasagna.go
+++ exercism/go/lasagna/lasagna.go
@@ -1,18 +1,18 @@
 package lasagna
 
-const OvenTime = 40
+var OvenTime = 40
 
 // RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
-func RemainingOvenTime(t int) int {
-	return OvenTime - t
+func RemainingOvenTime(actualMinutesInOven int) int {
+	return OvenTime - actualMinutesInOven
 }
 
 // PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
 func PreparationTime(numberOfLayers int) int {
-	return 2 * numberOfLayers
+	return numberOfLayers * 2
 }
 
 // ElapsedTime calculates the total time needed to create and bake a lasagna.
 func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
-	return PreparationTime(numberOfLayers) + actualMinutesInOven
+	return actualMinutesInOven + PreparationTime(numberOfLayers)
 }
