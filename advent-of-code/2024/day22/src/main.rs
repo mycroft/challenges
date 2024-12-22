41ab01d2549803e0f26c8a72c35cbd79bbbc9575
Patrick Marie <pm@mkz.me>
2024-12-22 22:00:09 +0100

    AOC 2024 day 22 (optimization).

--- advent-of-code/2024/day22/src/main.rs
+++ advent-of-code/2024/day22/src/main.rs
@@ -1,12 +1,12 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 
-fn compute(secret: i64) -> i64 {
+fn compute(secret: u64) -> u64 {
     let mut secret = ((secret * 64) ^ secret) % 16777216;
     secret = ((secret / 32) ^ secret) % 16777216;
     ((secret * 2048) ^ secret) % 16777216
 }
 
-fn read_input(fp: &str) -> Vec<i64> {
+fn read_input(fp: &str) -> Vec<u64> {
     std::fs::read_to_string(fp)
         .expect("Cannot read file")
         .lines()
@@ -14,7 +14,7 @@ fn read_input(fp: &str) -> Vec<i64> {
         .collect()
 }
 
-fn secrets(n: i64) -> Vec<i64> {
+fn secrets(n: u64) -> Vec<u64> {
     let mut secrets = Vec::new();
 
     secrets.push(n);
@@ -26,67 +26,73 @@ fn secrets(n: i64) -> Vec<i64> {
     secrets
 }
 
-fn prices(n: i64) -> Vec<i64> {
-    let mut prices = Vec::new();
-
+fn changes(base: &mut HashMap<u32, u64>, n: u64) {
+    // let mut result = HashSet::new();
+    let mut idx : u32 = 0;
     let mut secret = n;
 
-    prices.push(secret % 10);
+    let mut done = [false; 1 << 20];
 
-    for _ in 0..2000 {
+    for i in 0..2000 {
+        let old_secret = secret;
         secret = compute(secret);
-        prices.push(secret % 10);
-    }
 
-    prices
-}
+        let diff = (secret%10) as i32- (old_secret%10) as i32 + 9;
+        //println!("diff: {} idx {} idx {}", diff, idx, idx << 5);
+        idx <<= 5;
+        idx |= diff as u32;
+        idx %= 1 << 20;
 
-fn changes(prices: &[i64]) -> HashMap<VecDeque<i64>, i64> {
-    let mut current_change = VecDeque::new();
-    let mut result = HashMap::new();
-
-    for i in 1..prices.len() {
-        current_change.push_back(prices[i] - prices[i - 1]);
-        if current_change.len() > 4 {
-            current_change.pop_front();
-        }
-
-        if current_change.len() < 4 {
+        if i < 4 || done[idx as usize] {
             continue;
         }
 
-        if result.contains_key(&current_change) {
-            continue;
-        }
+        *base.entry(idx).or_insert(0) += secret % 10;
+        done[idx as usize] = true;
 
-        result.insert(current_change.clone(), prices[i]);
+        //result.insert(idx);
     }
-
-    result
 }
 
-fn solve_step2(numbers: &Vec<i64>) -> i64 {
-    let mut all_prices_changes_indexes: HashMap<VecDeque<i64>, i64> = HashMap::new();
-    
+fn solve_step2(numbers: &Vec<u64>) -> u64 {
+    let mut base: HashMap<u32, u64> = HashMap::new();
+
     for number in numbers {
-        let prices = prices(*number);
-        let changes = changes(&prices);
-
-        for (k, v) in changes.iter() {
-            *all_prices_changes_indexes.entry(k.clone()).or_insert(0) += v;
-        }
+        changes(&mut base, *number);
     }
-    
-    all_prices_changes_indexes.values().copied().max().unwrap()
+
+    base.values().copied().max().unwrap()
+}
+
+fn solve_step1(numbers: &Vec<u64>) -> u64 {
+    let secrets = numbers.iter().map(|x| secrets(*x)).collect::<Vec<_>>();
+    secrets.iter().map(|x| x[2000]).sum::<u64>()
 }
 
 fn main() {
     let input = read_input("input.txt");
-    
-    let secrets = input.iter().map(|x| secrets(*x)).collect::<Vec<_>>();
-    let result_step1 = secrets.iter().map(|x| x[2000]).sum::<i64>();
-    println!("#1 {}", result_step1);
+
+    let current_time = std::time::Instant::now();
+
+    let result_step1 = solve_step1(&input);
+    println!("#1 {}", result_step1); // 13764677935
+
+    println!("Time: {}ms", current_time.elapsed().as_millis());
+
 
     let result_step2 = solve_step2(&input);
-    println!("#2 {}", result_step2);
+    println!("#2 {}", result_step2); // 1619
+
+    println!("Time: {}ms", current_time.elapsed().as_millis());
+}
+
+#[test]
+fn sample() {
+    let input = read_input("input_test.txt");
+    let result_step1 = solve_step1(&input);
+    assert_eq!(result_step1, 37327623);
+
+    let input = read_input("input_test2.txt");
+    let result_step2 = solve_step2(&input);
+    assert_eq!(result_step2, 23);
 }
